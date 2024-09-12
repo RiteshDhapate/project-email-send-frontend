19a754c72ccbfcf2704d097c16ad2b818de774b0
ritesh <rieshdhapatepatil@gmail.com>
2024-09-12 14:26:00 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [emailBody, setEmailBody] = useState("");
+  const onSendEmail =async (e) => { 
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:8000/send-mail", {
+        email,
+        emailBody
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log({ email, emailBody });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contaner">
+      <form onSubmit={(e)=>onSendEmail(e)}>
+        <input
+          type="email"
+          placeholder="Enter email.."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <textarea
+          name=""
+          id=""
+          value={emailBody}
+          onChange={(e) => setEmailBody(e.target.value)}
+        ></textarea>
+        <br />
+        <br />
+        <button>Send Email</button>
+      </form>
     </div>
   );
 }
