d03fe20d7e55302eb9a83cfada3c7feff4f4bbb9
ritesh <rieshdhapatepatil@gmail.com>
2024-09-12 14:49:49 +0530

    added ai email genrater

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 import axios from "axios";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function App() {
   const [email, setEmail] = useState("");
   const [emailBody, setEmailBody] = useState("");
-  const onSendEmail =async (e) => { 
+  const [userPrompt, setUserPrompt] = useState("");
+
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyBKZatzIoctt0_arla80wDMgGmZP202jHw"
+  );
+  const sendPrompt = async () => {
+    try {
+      console.log("sending server prompt");
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+      const prompt = `
+      genrate the one email for me
+      prompt = ${userPrompt}
+      `;
+
+      console.log(userPrompt);
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+      setEmailBody(text);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSendEmail = async (e) => {
     e.preventDefault();
     try {
       const result = await axios.post("http://localhost:8000/send-mail", {
         email,
-        emailBody
+        emailBody,
       });
       console.log(result);
     } catch (error) {
       console.log(error);
     }
     console.log({ email, emailBody });
-  }
+  };
   return (
     <div className="contaner">
-      <form onSubmit={(e)=>onSendEmail(e)}>
+      <input type="text" placeholder="Enter Prompt to genrate the email" />
+      <br />
+      <br />
+      <br />
+      <button onClick={sendPrompt}>Genrate email</button>
+      <br />
+      <br />
+      <form onSubmit={(e) => onSendEmail(e)}>
+        <br />
         <input
           type="email"
           placeholder="Enter email.."
